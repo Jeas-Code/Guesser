f343b2cde383944a71e045e1630801e72f2511a0
Jeas-Code <1794837325@qq.com>
2021-05-23 13:39:28 +0800

    add Notification

--- app/src/main/java/com/example/jeas/netapp/MainActivity.java
+++ app/src/main/java/com/example/jeas/netapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.jeas.netapp;
 
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button exit_game_btn;
 
     @Override
+    @RequiresApi(api = 26)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -26,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(actionBar != null){
             actionBar.hide();
             }
+        //弹出邀请好友游戏通知
+        notice();
 
         start_game_btn = (Button) findViewById(R.id.start_game_btn);
         invite_friends_btn = (Button) findViewById(R.id.invite_friends_btn);
@@ -82,6 +94,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
+
+    //进入游戏通知实现
+    @RequiresApi(api = 26)
+    public void notice() {
+                String id = "001";
+                String name="Guesser";
+                Notification notification = null;
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Intent intent = new Intent(this, Notification.class);
+                PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel mChannel = new NotificationChannel(id, name,
+                            NotificationManager.IMPORTANCE_LOW);
+                    Toast.makeText(this, mChannel.toString(), Toast.LENGTH_SHORT).show();
+                    notificationManager.createNotificationChannel(mChannel);
+                    notification = new Notification.Builder(this)
+                            .setChannelId(id)
+                            .setContentTitle("欢迎来到Guesser !!!")
+                            .setContentText("一个人玩无聊？快来邀请好友同玩互动吧！！")
+                            .setWhen(System.currentTimeMillis())
+                            .setSmallIcon(R.mipmap.ic_love)
+                            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.game_notice_img1))
+                            .setContentIntent(pi)
+                            .setAutoCancel(true)
+                            .setDefaults(Notification.DEFAULT_ALL)
+                            .setStyle(new Notification.BigPictureStyle().bigPicture(
+                                    BitmapFactory.decodeResource(getResources(), R.drawable.game_notice_img2)))
+                            .setPriority(Notification.PRIORITY_MAX)
+                            .build();
+                } else {
+                    notification = new NotificationCompat.Builder(this)
+                            .setContentTitle("冬去春来，好久不见~")
+                            .setContentText("LuLu baby, You are the one Who I love❤")
+                            .setWhen(System.currentTimeMillis())
+                            .setSmallIcon(R.mipmap.ic_love)
+                            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.game_notice_img3))
+                            .setOngoing(true)
+                            .setChannel(id)
+                            .build();//无效
+                }
+                notificationManager.notify(1, notification);
+        }
 
     @Override
     protected void onNewIntent(Intent intent) {
