15210c0295173303b30b68c292b23ab25809d7cc
Jeas-Code <1794837325@qq.com>
2021-06-03 22:59:32 +0800

    add clear panal and save work function

--- app/src/main/java/com/example/jeas/netapp/Draw.java
+++ app/src/main/java/com/example/jeas/netapp/Draw.java
@@ -1,16 +1,24 @@
 package com.example.jeas.netapp;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class Draw extends AppCompatActivity {
 
@@ -18,6 +26,11 @@ public class Draw extends AppCompatActivity {
 
     private ImageView imageView;
     private Bitmap mbitmap;
+    Canvas canvas = new Canvas(mbitmap);
+    Paint paint = new Paint();
+
+    private Button reset_panal_btn;
+    private Button save_pic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,9 @@ public class Draw extends AppCompatActivity {
 
         imageView = (ImageView)findViewById(R.id.draw_img);
 
+        reset_panal_btn = (Button)findViewById(R.id.reset_panal_btn);
+        save_pic = (Button)findViewById(R.id.save_picture_btn);
+
         //隐藏默认的标题栏
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
@@ -36,11 +52,8 @@ public class Draw extends AppCompatActivity {
         }
 
         //Bitmap bitmap  = BitmapFactory.decodeResource(getResources(), R.drawable.game_notice_img2);
-        mbitmap = Bitmap.createBitmap(1000, 1200, Bitmap.Config.ARGB_8888);
+        mbitmap = Bitmap.createBitmap(imageView.getWidth(), imageView.getHeight(), Bitmap.Config.ARGB_8888);
 
-        final Canvas canvas = new Canvas(mbitmap);
-
-        final Paint paint = new Paint();
         paint.setColor(Color.BLACK);
         paint.setStrokeWidth(12);
         paint.setAntiAlias(true);
@@ -66,8 +79,8 @@ public class Draw extends AppCompatActivity {
                             int x = (int)event.getX();
                             int y = (int)event.getY();
                             //Toast.makeText(Draw.this, x+"-"+y, Toast.LENGTH_SHORT).show();
-                            for(int i=-10; i<10; i++){
-                                for (int j = -10; j<10; j++) {
+                            for(int i=-10; i<=10; i++){
+                                for (int j = -10; j<=10; j++) {
                                     if (Math.sqrt((i * i) + (j * j)) <= 10) {
                                         //mbitmap.setPixel(x + i, y + j, Color.BLACK);
                                         canvas.drawPoint(x+i, y+j, paint);
@@ -96,5 +109,35 @@ public class Draw extends AppCompatActivity {
 
 
     }
+
+    protected void resetPanel(){
+        if(mbitmap != null){
+            mbitmap = Bitmap.createBitmap(imageView.getWidth(), imageView.getHeight(), Bitmap.Config.ARGB_8888);
+            canvas = new Canvas(mbitmap);
+            canvas.drawColor(Color.WHITE);
+            imageView.setImageBitmap(mbitmap);
+            Toast.makeText(Draw.this, "清除画板成功!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void saveBitmap(){
+        try{
+            File file = new File(Environment.getExternalStorageDirectory(),
+                    System.currentTimeMillis()+".png");
+            FileOutputStream stream = new FileOutputStream(file);
+            mbitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            Toast.makeText(Draw.this, "成功保存图片!", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_MEDIA_MOUNTED);
+            intent.setData(Uri.fromFile(Environment.getDownloadCacheDirectory()));
+            sendBroadcast(intent);
+
+        }catch (Exception e){
+            Toast.makeText(Draw.this, "保存图片失败!!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
 
 }
