d08221e53ccb5356a781a807467d5e1d75362fa2
Jeas-Code <1794837325@qq.com>
2021-06-06 22:40:23 +0800

    add socket communication

--- app/src/main/java/com/example/jeas/netapp/Chat.java
+++ app/src/main/java/com/example/jeas/netapp/Chat.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,8 +14,17 @@ import android.widget.Toast;
 import com.example.jeas.netapp.adapter.MsgAdapter;
 import com.example.jeas.netapp.media.Msg;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Chat extends AppCompatActivity {
 
@@ -24,10 +34,31 @@ public class Chat extends AppCompatActivity {
     private RecyclerView msgRecycleView;
     private MsgAdapter adapter;
 
+    //初始化Socket通信所需的类型
+    private EditText mEditText;
+    //private TextView mTextView;
+    private static final String TAG = "TAG";
+    private static final String HOST = "192.168.137.1";
+    private static final int PORT = 8000;
+    private PrintWriter printWriter;
+    private BufferedReader in;
+    private ExecutorService mExecutorService = null;
+    private String receiveMsg;
+    //private SocketTest socketTest;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        //启动服务器
+        //socketTest = new SocketTest();
+
+        mEditText = (EditText) findViewById(R.id.input_text);
+        //mTextView = (TextView) findViewById(R.id.textView);
+        mExecutorService = Executors.newCachedThreadPool();
+
         initMsgs();
 
         ActionBar actionBar = getSupportActionBar();
@@ -81,5 +112,76 @@ public class Chat extends AppCompatActivity {
         msgList.add(msg4);
         Msg msg5 = new Msg("How are you bro?", Msg.TYPE_RECEIVED);
         msgList.add(msg5);
+    }
+
+    //实现Socket客户端通信的函数
+    public void connect(View view) {
+        mExecutorService.execute(new connectService());  //在一个新的线程中请求 Socket 连接
+    }
+
+    public void send(View view) {
+        String sendMsg = mEditText.getText().toString();
+        mExecutorService.execute(new sendService(sendMsg));
+    }
+
+    public void disconnect(View view) {
+        mExecutorService.execute(new sendService("0"));
+    }
+
+    private class sendService implements Runnable {
+        private String msg;
+
+        sendService(String msg) {
+            this.msg = msg;
+        }
+
+        @Override
+        public void run() {
+            printWriter.println(this.msg);
+        }
+    }
+
+    private class connectService implements Runnable {
+        @Override
+        public void run() {//可以考虑在此处添加一个while循环，结合下面的catch语句，实现Socket对象获取失败后的超时重连，直到成功建立Socket连接
+            try {
+                Socket socket = new Socket(HOST, PORT);      //步骤一
+                socket.setSoTimeout(5000);
+                printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(   //步骤二
+                        socket.getOutputStream(), "UTF-8")), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                receiveMsg();
+            } catch (Exception e) {
+                Log.e(TAG, ("connectService:" + e.getMessage()));   //如果Socket对象获取失败，即连接建立失败，会走到这段逻辑
+            }
+        }
+    }
+
+    private void receiveMsg() {
+        try {
+            while (true) {                                      //步骤三
+                if ((receiveMsg = in.readLine()) != null) {
+                    Log.d(TAG, "receiveMsg:" + receiveMsg);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //mTextView.setText(receiveMsg + "\n\n" + mTextView.getText());
+                            Msg msg = new Msg(receiveMsg, Msg.TYPE_RECEIVED);
+                            msgList.add(msg);
+
+                        }
+                    });
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "receiveMsg: ");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disconnect(mEditText);
     }
 }
