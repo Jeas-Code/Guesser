ace19b76cf01aa822e4462f16ac252bbd3d13f9e
Jeas-Code <1794837325@qq.com>
2021-06-07 13:13:05 +0800

    http again

--- app/src/main/java/com/example/jeas/netapp/Chat.java
+++ app/src/main/java/com/example/jeas/netapp/Chat.java
@@ -5,7 +5,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +13,11 @@ import android.widget.Toast;
 import com.example.jeas.netapp.adapter.MsgAdapter;
 import com.example.jeas.netapp.media.Msg;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import static com.example.jeas.netapp.media.Msg.TYPE_RECEIVED;
 
 public class Chat extends AppCompatActivity {
 
@@ -34,21 +27,16 @@ public class Chat extends AppCompatActivity {
     private RecyclerView msgRecycleView;
     private MsgAdapter adapter;
 
+
+    private String host="localhost";
+    private int port=8000;
+    private Socket socket;
+
     //初始化Socket通信所需的类型
     private EditText mEditText;
-    //private TextView mTextView;
-    private static final String TAG = "TAG";
-    private static final String HOST = "192.168.137.1";
-    private static final int PORT = 8000;
-    private PrintWriter printWriter;
-    private BufferedReader in;
-    private ExecutorService mExecutorService = null;
-    private String receiveMsg;
-    //private SocketTest socketTest;
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
@@ -57,7 +45,7 @@ public class Chat extends AppCompatActivity {
 
         mEditText = (EditText) findViewById(R.id.input_text);
         //mTextView = (TextView) findViewById(R.id.textView);
-        mExecutorService = Executors.newCachedThreadPool();
+        //mExecutorService = Executors.newCachedThreadPool();
 
         initMsgs();
 
@@ -75,6 +63,13 @@ public class Chat extends AppCompatActivity {
         msgRecycleView.setLayoutManager(layoutManager);
         adapter = new MsgAdapter(msgList);
         msgRecycleView.setAdapter(adapter);
+
+//        try {
+//            socket=new Socket(host,port);
+//            talk();
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
 
         //设置发送按钮点击事件
         send.setOnClickListener(new View.OnClickListener() {
@@ -102,86 +97,133 @@ public class Chat extends AppCompatActivity {
     }
 
     private void initMsgs() {
-        Msg msg1 = new Msg("Hello guy!", Msg.TYPE_RECEIVED);
+        Msg msg1 = new Msg("Hello guy!", TYPE_RECEIVED);
         msgList.add(msg1);
         Msg msg2 = new Msg("Hello, Who is that?", Msg.TYPE_SENT);
         msgList.add(msg2);
-        Msg msg3 = new Msg("This is Tom, Nice to meet you.", Msg.TYPE_RECEIVED);
+        Msg msg3 = new Msg("This is Tom, Nice to meet you.", TYPE_RECEIVED);
         msgList.add(msg3);
         Msg msg4 = new Msg("Nice to meet you to!", Msg.TYPE_SENT);
         msgList.add(msg4);
-        Msg msg5 = new Msg("How are you bro?", Msg.TYPE_RECEIVED);
+        Msg msg5 = new Msg("How are you bro?", TYPE_RECEIVED);
         msgList.add(msg5);
     }
 
-    //实现Socket客户端通信的函数
-    public void connect(View view) {
-        mExecutorService.execute(new connectService());  //在一个新的线程中请求 Socket 连接
-    }
 
-    public void send(View view) {
-        String sendMsg = mEditText.getText().toString();
-        mExecutorService.execute(new sendService(sendMsg));
-    }
+//    public Chat()throws IOException{
+//        socket=new Socket(host,port);
+//    }
+//
+//    private PrintWriter getWriter(Socket socket)throws IOException {
+//        OutputStream socketOut = socket.getOutputStream();
+//        return new PrintWriter(socketOut,true);
+//    }
+//    private BufferedReader getReader(Socket socket)throws IOException{
+//        InputStream socketIn = socket.getInputStream();
+//        return new BufferedReader(new InputStreamReader(socketIn));
+//    }
+//
+//    public void talk()throws IOException {
+//        try{
+//            BufferedReader br=getReader(socket);
+//            PrintWriter pw=getWriter(socket);
+//            BufferedReader localReader=new BufferedReader(new InputStreamReader(System.in));
+//            String recemsg = inputText.getText().toString();
+//            final String content = br.readLine();
+//            while(recemsg != null){
+//
+//                pw.println(recemsg);
+//                System.out.println(br.readLine());
+//                //如果服务器输入内容不为空，则可接受该信息
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//            public void run() {
+//                Msg msg = new Msg(content, Msg.TYPE_RECEIVED);
+//                msgList.add(msg);
+//                //刷新新消息的显示
+//                adapter.notifyItemInserted(msgList.size() - 1);
+//                //将布局定位到最后一行最新的消息上
+//                msgRecycleView.scrollToPosition(msgList.size() - 1);
+//                              }
+//                    });
+//                if(recemsg.equals("bye")) {
+//                    break;
+//                }
+//
+//                }
+//
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        }finally{
+//            try{socket.close();}catch(IOException e){e.printStackTrace();}
+//        }
+//    }
 
-    public void disconnect(View view) {
-        mExecutorService.execute(new sendService("0"));
-    }
+//    //实现Socket客户端通信的函数
+//    public void connect(View view) {
+//        mExecutorService.execute(new connectService());  //在一个新的线程中请求 Socket 连接
+//    }
+//
+//    public void send(View view) {
+//        String sendMsg = mEditText.getText().toString();
+//        mExecutorService.execute(new sendService(sendMsg));
+//    }
+//
+//    public void disconnect(View view) {
+//        mExecutorService.execute(new sendService("0"));
+//    }
+//
+//    private class sendService implements Runnable {
+//        private String msg;
+//
+//        sendService(String msg) {
+//            this.msg = msg;
+//        }
+//
+//        @Override
+//        public void run() {
+//            printWriter.println(this.msg);
+//        }
+//    }
+//
+//    private class connectService implements Runnable {
+//        @Override
+//        public void run() {//可以考虑在此处添加一个while循环，结合下面的catch语句，实现Socket对象获取失败后的超时重连，直到成功建立Socket连接
+//            try {
+//                Socket socket = new Socket(HOST, PORT);      //步骤一
+//                socket.setSoTimeout(5000);
+//                printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(   //步骤二
+//                        socket.getOutputStream(), "UTF-8")), true);
+//                in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+//                receiveMsg();
+//            } catch (Exception e) {
+//                Log.e(TAG, ("connectService:" + e.getMessage()));   //如果Socket对象获取失败，即连接建立失败，会走到这段逻辑
+//            }
+//        }
+//    }
+//
+//    private void receiveMsg() {
+//        try {
+//            while (true) {                                      //步骤三
+//                if ((receiveMsg = in.readLine()) != null) {
+//                    Log.d(TAG, "receiveMsg:" + receiveMsg);
+//                    runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            //mTextView.setText(receiveMsg + "\n\n" + mTextView.getText());
+//                            Msg msg = new Msg(receiveMsg, Msg.TYPE_RECEIVED);
+//                            msgList.add(msg);
+//
+//                        }
+//                    });
+//                }
+//            }
+//        } catch (IOException e) {
+//            Log.e(TAG, "receiveMsg: ");
+//            e.printStackTrace();
+//        }
+//    }
 
-    private class sendService implements Runnable {
-        private String msg;
 
-        sendService(String msg) {
-            this.msg = msg;
-        }
 
-        @Override
-        public void run() {
-            printWriter.println(this.msg);
-        }
-    }
-
-    private class connectService implements Runnable {
-        @Override
-        public void run() {//可以考虑在此处添加一个while循环，结合下面的catch语句，实现Socket对象获取失败后的超时重连，直到成功建立Socket连接
-            try {
-                Socket socket = new Socket(HOST, PORT);      //步骤一
-                socket.setSoTimeout(5000);
-                printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(   //步骤二
-                        socket.getOutputStream(), "UTF-8")), true);
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-                receiveMsg();
-            } catch (Exception e) {
-                Log.e(TAG, ("connectService:" + e.getMessage()));   //如果Socket对象获取失败，即连接建立失败，会走到这段逻辑
-            }
-        }
-    }
-
-    private void receiveMsg() {
-        try {
-            while (true) {                                      //步骤三
-                if ((receiveMsg = in.readLine()) != null) {
-                    Log.d(TAG, "receiveMsg:" + receiveMsg);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            //mTextView.setText(receiveMsg + "\n\n" + mTextView.getText());
-                            Msg msg = new Msg(receiveMsg, Msg.TYPE_RECEIVED);
-                            msgList.add(msg);
-
-                        }
-                    });
-                }
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "receiveMsg: ");
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        disconnect(mEditText);
-    }
 }
---
+++ app/src/main/java/com/example/jeas/netapp/service/EchoServer.java
@@ -0,0 +1,133 @@
+package com.example.jeas.netapp.service;
+
+/**
+ * Created by Jeas on 2021/6/7.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    private int port=8000;
+    private ServerSocket serverSocket;
+
+    public EchoServer() throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("服务器启动");
+    }
+
+    public void service() {
+        while (true) {
+            Socket socket=null;
+            try {
+                socket = serverSocket.accept();  //接收客户连接
+                Thread workThread=new Thread(new Handler(socket));  //创建一个工作线程
+                workThread.start();  //启动工作线程
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String args[])throws IOException {
+        new EchoServer().service();
+    }
+}
+
+class Handler implements Runnable{
+    private Socket socket;
+
+    public Handler(Socket socket){
+        this.socket=socket;
+    }
+    private PrintWriter getWriter(Socket socket)throws IOException{
+        OutputStream socketOut = socket.getOutputStream();
+        return new PrintWriter(socketOut,true);
+    }
+    private BufferedReader getReader(Socket socket)throws IOException{
+        InputStream socketIn = socket.getInputStream();
+        return new BufferedReader(new InputStreamReader(socketIn));
+    }
+    public String echo(String msg) {
+        return "echo:" + msg;
+    }
+    public void run(){
+
+        System.out.println("New connection accepted " +
+                socket.getInetAddress() + ":" +socket.getPort());
+
+        try {
+            new Thread(new Runnable() {
+                BufferedReader br =getReader(socket);
+                PrintWriter pw = getWriter(socket);
+                BufferedReader localReader=new BufferedReader(new InputStreamReader(System.in));
+                String msg=null;
+                @Override
+                public void run() {
+                    // TODO Auto-generated method stub
+                    try {
+                        while((msg = localReader.readLine())!=null){
+
+                            //客户端传给服务器的字符串msg
+                            pw.println(msg);
+                            System.out.println(br.readLine());
+                            pw.flush();
+
+                            if(msg.equals("bye"))
+                                break;
+
+                            msg = null;
+                        }
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    BufferedReader br =getReader(socket);
+                    PrintWriter pw = getWriter(socket);
+                    BufferedReader localReader=new BufferedReader(new InputStreamReader(System.in));
+
+                    String msg = null;
+
+                    while((msg = br.readLine())!= null){
+
+                        //服务器端传给客户端的字符串echoe(msg)
+                        pw.println(echo(msg));
+                        System.out.println(msg);
+                        pw.flush();
+
+                        if (msg.equals("bye"))
+                            break;
+                    }
+
+
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    try{
+                        if(socket!=null)socket.close();
+                    }catch (IOException e) {e.printStackTrace();}
+                }
+            }}).start();
+
+    }
+}
